b67ef978d130c5afd0e7383a30ff0bed062caf44
dcheng <cheng.dorothy.s@gmail.com>
2021-05-04 17:30:58 -0600

    Added new/post routes for Photos

--- controllers/photo_controllers.js
+++ controllers/photo_controllers.js
@@ -1,13 +1,39 @@
 const express=require('express');
+const Photo = require('../models/Photo');
 const router=express.Router();
 const User=require('../models/User');
+router.use(express.urlencoded({extended:false}));
 // ROUTES
 // Index
 router.get('/',async (req,res)=>{
-    const allUsers= await User.find();
+    const allPhotos=await Photo.find();
     res.render('photos/index',{
-        users: allUsers,
+        photos: allPhotos,
     });
+});
+// New
+router.get('/new', (req,res)=>{
+    res.render('photos/new');
+})
+// Post
+router.post('/',async (req,res)=>{
+    const username=req.body.username;
+    const user= await User.findOne({username:username});
+    if (! user) {
+        res.send('error: user doesn\'t exist');
+    } else {
+        const userId=user._id;
+        const photoUrl=req.body.url;
+        const photoAbout=req.body.about;
+        const newPhoto=await Photo.create({
+            url: photoUrl,
+            about: photoAbout,
+            user: userId,
+        });
+        user.photos.push(newPhoto);
+        await user.save();
+        res.redirect(`/user/${userId}`);
+    };
 });
 
 module.exports=router;
--- models/Photo.js
+++ models/Photo.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const photoSchema= new mongoose.Schema({
     url: {type: String, required: true},
     about: String,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
 },{timestamps: true});
 
 const Photo=mongoose.model('Photo',photoSchema);
