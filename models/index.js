43fb57abefd2a2099492191e920f80f7660edf83
dcheng <cheng.dorothy.s@gmail.com>
2021-05-14 14:22:07 -0600

    Updated mongoose connection error message

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ mongoose.connect(dbUrl,{
     useFindAndModify:false,
 });
 mongoose.connection.on('connected',()=>console.log(`Mongoose connected on ${dbUrl}`));
-mongoose.connection.on('error',()=>console.log(`Mongoose error: ${error}`));
+mongoose.connection.on('error',(error)=>console.log(`Mongoose error: ${error}`));
 mongoose.connection.on('disconnected',()=>console.log('Mongoose disconnected'));
 
 module.exports={
